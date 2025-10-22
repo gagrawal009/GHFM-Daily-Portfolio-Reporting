4d7857d3023f025e341d7fbb9ad875fd17d4e030
Gaurav Agrawal <g.agrawal009@gmail.com>
2025-10-22 13:34:00 +0800

    Files upload

--- 2. Reporting_Code/Run_Daily_Report.py
+++ 2. Reporting_Code/Run_Daily_Report.py
@@ -1,7 +1,7 @@
 from Report_Module_Automated import PortfolioReportingFramework
 
 ghfm_reporting_dir = "./"
-repo_path = r"C:\Users\Guarav\OneDrive - Golden Horse Fund Management Pte. Ltd\Investment Team's files - Investments Team\GHFM-Daily-Portfolio-Reporting"
+repo_path = r"C:\Users\Guarav\OneDrive - Golden Horse Fund Management Pte. Ltd\Investments Team\GHFM-Daily-Portfolio-Reporting"
 
 reporter = PortfolioReportingFramework(ghfm_reporting_dir, repo_path)
 reporter.run_complete_daily_report()

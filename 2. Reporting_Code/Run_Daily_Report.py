39c5cbd1900f5698e1b15142817c0b6476e453eb
Gaurav Agrawal <g.agrawal009@gmail.com>
2026-05-06 14:37:31 +0800

    Files upload

--- 2. Reporting_Code/Run_Daily_Report.py
+++ 2. Reporting_Code/Run_Daily_Report.py
@@ -6,7 +6,11 @@ reporter = PortfolioReportingFramework(ghfm_reporting_dir, repo_path)
 reporter.run_complete_daily_report()
 
 # from Report_Module_Automated_with_Date import PortfolioReportingFramework
-# today_str = "20251021"
-# previous_day_str = "20251020"
+today_str = "20260429"
+previous_day_str = "20260428"
 # reporter = PortfolioReportingFramework(today_str, previous_day_str, ghfm_reporting_dir, repo_path)
+# reporter.run_complete_daily_report()
+
+# from Report_Module_Automated_Download import PortfolioReportingFrameworkDownload
+# reporter = PortfolioReportingFrameworkDownload(today_str, previous_day_str, ghfm_reporting_dir)
 # reporter.run_complete_daily_report()

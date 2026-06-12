16cb57eb7f411cd2b4bd9ae96716d9156420ed15
Gaurav Agrawal <g.agrawal009@gmail.com>
2026-06-12 11:03:25 +0800

    Files upload

--- 2. Reporting_Code/ibkr_tickers.py
+++ 2. Reporting_Code/ibkr_tickers.py
@@ -8,8 +8,8 @@ import io
 import csv
 
 SEND_REQUEST_URL = "https://ndcdyn.interactivebrokers.com/AccountManagement/FlexWebService/SendRequest"
-#GET_STATEMENT_URL = "https://ndcdyn.interactivebrokers.com/AccountManagement/FlexWebService/GetStatement"
-GET_STATEMENT_URL = "https://gdcdyn.interactivebrokers.com/AccountManagement/FlexWebService/GetStatement"
+GET_STATEMENT_URL = "https://ndcdyn.interactivebrokers.com/AccountManagement/FlexWebService/GetStatement"
+#GET_STATEMENT_URL = "https://gdcdyn.interactivebrokers.com/AccountManagement/FlexWebService/GetStatement"
 
 
 DEFAULT_TIMEOUT = 30
